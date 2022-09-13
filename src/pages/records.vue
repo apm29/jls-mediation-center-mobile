475c20c9eb849827dc2d5b05c0dbf14ce56dc252
apm29 <yjw999wow@163.com>
2022-09-13 17:27:22 +0800

    fix: redirect

---
+++ src/pages/records.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { useRouter, useRoute } from "~/composables";
+const router = useRouter();
+const route = useRoute();
+router.replace({
+  path: "/app/records",
+  query: route.query,
+});
+</script>
+
+<style lang="scss" scoped></style>
